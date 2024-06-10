c39c463bb27f9a4959d855474e4579c7480b2b21
Kheyda <khanyamelapi@gmail.com>
2024-06-10 08:38:23 +0200

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/khanya.jpg" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+            Hello, and welcome to my portfolio! I am a driven professional with a foundation in ICT from Walter Sisulu University, supplemented by two Python certificates and invaluable experience gained at Samsung Innovation Campus. After graduating with an Advanced Diploma in ICT in Application Development, I embarked on a fulfilling journey in the software development industry. Currently, I'm pursuing my dreams and advancing my career at Capaciti UVU Africa, where I specialize in crafting innovative software solutions.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
