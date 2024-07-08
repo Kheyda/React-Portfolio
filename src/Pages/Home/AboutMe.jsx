78b377f6e3438c492e5f36500aa7ce5e372748f1
Kheyda <khanyamelapi@gmail.com>
2024-07-08 14:58:32 +0200

    new changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,18 +1,24 @@
 export default function AboutMe() {
-    return (
-      <section id="AboutMe" className="about--section">
-        <div className="about--section--img">
-          <img src="./img/khanya.jpg" alt="About Me" />
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/Khanya Somagaca.jpg" alt="About Me" />
+      </div>
+      <div className="about--section--content">
+        <h1 className="about--section--heading">About Me</h1>
+        <div className="about--section--info">
+          <p><strong>Name:</strong> Khanya Somagaca</p>
+          <p><strong>Birthday:</strong> 28 Dec</p>
+          <p><strong>Phone:</strong> 0738697648</p>
+          <p><strong>Email:</strong> khanyamelapi@gmail.com</p>
+          <p><strong>Address:</strong> 389E Nassau road, Wynberg, Cape Town</p>
         </div>
-        <div className="hero--section--content--box about--section--box">
-          <div className="hero--section--content">
-            <h1 className="skills-section--heading">About Me</h1>
-            <p className="hero--section-description">
-            Hello, and welcome to my portfolio! I am a driven professional with a foundation in ICT from Walter Sisulu University, supplemented by two Python certificates and invaluable experience gained at Samsung Innovation Campus. After graduating with an Advanced Diploma in ICT in Application Development, I embarked on a fulfilling journey in the software development industry. Currently, I'm pursuing my dreams and advancing my career at Capaciti UVU Africa, where I specialize in crafting innovative software solutions.
-            </p>
-          </div>
-        </div>
-      </section>
-    );
-  }
-  
+        <p className="about--section--description">
+          Hello, and welcome to my portfolio! I am a driven professional with a foundation in ICT from Walter Sisulu University, supplemented by two Python certificates and invaluable experience gained at Samsung Innovation Campus. After graduating with an Advanced Diploma in ICT in Application Development, I embarked on a fulfilling journey in the software development industry. Currently, I'm pursuing my dreams and advancing my career at Capaciti UVU Africa, where I specialize in crafting innovative software solutions.
+          <br /><br />
+          I became a full stack developer because I am passionate about creating innovative solutions and improving user experiences. The most rewarding aspect of my profession is collaborating with talented teams to solve complex problems and deliver high-quality software that meets users' needs. I value communication and teamwork, and I thrive in environments where I can contribute my skills to assist both people and the company in achieving their goals.
+        </p>
+      </div>
+    </section>
+  );
+}
